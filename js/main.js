cffb3ccc65a6ccf296cccdbbd276d2750171b7f4
Risma Dhiva Syabila <risma.dhivasyabilla@gmail.com>
2024-05-16 11:30:14 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.hamburger');
+const bars = document.querySelector('.icon-bars');
+const close = document.querySelector('.icon-close');
+
+hamburger.addEventListener('click',displayMenu);
+menu.addEventListener('click',displayMenu);
+
+function displayMenu(){
+    if(menu.classList.contains('tampil')){
+        menu.classList.remove('tampil');
+        bars.style.display='inline';
+        close.style.display='none';
+    } else{
+        menu.classList.add('tampil');
+        bars.style.display='none';
+        close.style.display='inline';
+    }
+}
